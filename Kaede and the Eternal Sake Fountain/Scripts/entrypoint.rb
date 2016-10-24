8df05bfed45534e2c2f94deeb01f81e46ba1f28f
momo <milkythunderous@gmail.com>
2016-10-24 17:58:53 -0400

    unpacked kaede YAML files
    hopefully correctly

--- Kaede and the Eternal Sake Fountain/Scripts/entrypoint.rb
+++ Kaede and the Eternal Sake Fountain/Scripts/entrypoint.rb
@@ -2,6 +2,10 @@ $LOAD_PATH << '.'
 $LOAD_PATH << 'Scripts'
 $LOAD_PATH << 'Scripts/RPG'
 
+ofile = File.open("something.txt", "w")
+ofile.write("ROFFLEWAFFLES")
+ofile.close()
+
 require "RPG"
 
 rgss_main { SceneManager.run }
