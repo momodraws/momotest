e5c622261e9f740c480af57df1fffee7f9d4bddd
momo <milkythunderous@gmail.com>
2016-10-24 20:53:35 -0400

    added new maps to test
    kaede's secret desert wine cellar and desert hideout

--- Kaede and the Eternal Sake Fountain/pack.bat
+++ Kaede and the Eternal Sake Fountain/pack.bat
@@ -1,5 +1,6 @@
 @echo off
 echo packing RPG Maker game at: "%~dp0."
+echo if this is okay, press any key!
+pause
 rvpacker --action pack --project "%~dp0." --project-type ace
 echo done!
-pause
--- Kaede and the Eternal Sake Fountain/unpack.bat
+++ Kaede and the Eternal Sake Fountain/unpack.bat
@@ -1,5 +1,6 @@
 @echo off
 echo unpacking RPG Maker game at: "%~dp0."
+pause
 rvpacker --action unpack --project "%~dp0." --project-type ace
 echo done!
 pause
